6bad11f8599442eb4064839381e26720f0ccf937
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-09-21 19:24:01 +0900

    ADD: 메인페이지 작업 중

---
+++ client/src/app/main/MainForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SearchComponent from "@/components/SearchComponent";
+
+export default function MainForm() {
+  return (
+    <div className="max-w-[600px] w-full text-black">
+      <SearchComponent />
+    </div>
+  );
+}
---
+++ client/src/app/main/page.tsx
@@ -0,0 +1,6 @@
+import PageContainer from "@/components/PageContainer";
+import MainForm from "./MainForm";
+
+export default function MainPage() {
+  return <PageContainer children={<MainForm />} />;
+}
