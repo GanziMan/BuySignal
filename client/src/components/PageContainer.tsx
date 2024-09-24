6a9f92474544ea3745ebd74b7f192e286e0a0eb1
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-09-24 13:23:10 +0900

    Fix: PageContainer style 수정

--- client/src/components/PageContainer.tsx
+++ client/src/components/PageContainer.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function PageContainer({ children }: { children: ReactNode }) {
   return (
-    <main className="w-full h-screen flex justify-center bg-slate-100">
+    <main className="w-full h-full flex justify-center bg-slate-100">
       <section className="max-w-[600px] w-full bg-white">{children}</section>
     </main>
   );
