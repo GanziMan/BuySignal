67cf8564f0c0d4d6fc713552ba8a89667a3d2b1d
GanziMan <qjatn50089@gmail.com>
2024-09-25 22:47:46 +0900

    Fix: Mainform수정

--- client/src/app/main/MainForm.tsx
+++ client/src/app/main/MainForm.tsx
@@ -31,7 +31,7 @@ const PROUDCT_DATA: ProductDataType[] = [
 ];
 export default function MainForm() {
   return (
-    <div className="w-full h-full flex-auto flex flex-col items-center gap-5 text-black p-5">
+    <div className="w-full h-full flex-auto flex flex-col items-center gap-5 text-black py-5">
       <Image
         src={"/images/location/img-location.png"}
         alt="위치 이미지"
