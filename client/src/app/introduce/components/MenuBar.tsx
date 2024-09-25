e7cd0aa22ffcf987699fa9fa104594561d164b6a
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-09-25 16:40:50 +0900

    ADD: 소개 페이지 추가

---
+++ client/src/app/introduce/components/MenuBar.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { motion, Variants } from "framer-motion";
+import { useRouter } from "next/navigation";
+
+const itemVariants: Variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+
+      duration: 0.2,
+      damping: 24,
+    },
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+};
+
+export default function MenuBar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const router = useRouter();
+  return (
+    <motion.nav
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      className="menu"
+      style={{
+        filter: "drop-shadow(1px 1px 1px #f1e0ce)",
+        width: 300,
+      }}
+    >
+      <motion.button
+        whileTap={{ scale: 0.97 }}
+        onClick={() => setIsOpen(!isOpen)}
+        style={{
+          WebkitAppearance: "button",
+          background: "white",
+          color: "black",
+          border: "2px solid black",
+          borderRadius: "10px",
+          padding: "10px 20px",
+          fontSize: "16px",
+          fontWeight: 500,
+          cursor: "pointer",
+          width: "100%",
+          textAlign: "center",
+          marginBottom: "10px",
+
+          height: 47,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        목록
+      </motion.button>
+      <motion.ul
+        variants={{
+          open: {
+            clipPath: "inset(0% 0% 0% 0% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.7,
+              delayChildren: 0.3,
+              staggerChildren: 0.05,
+            },
+          },
+          closed: {
+            clipPath: "inset(10% 50% 90% 50% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.3,
+            },
+          },
+        }}
+        style={{
+          pointerEvents: isOpen ? "auto" : "none",
+          listStyle: "none",
+          margin: 0,
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          textAlign: "center",
+        }}
+      >
+        <motion.li style={framerListyle} variants={itemVariants}>
+          로그인
+        </motion.li>
+        <motion.li
+          style={framerListyle}
+          variants={itemVariants}
+          onClick={() => router.push("/main")}
+        >
+          상품 보기
+        </motion.li>
+      </motion.ul>
+    </motion.nav>
+  );
+}
+
+const framerListyle = {
+  height: "50px",
+  border: "1px solid black",
+  borderRadius: "10px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "black",
+  fontWeight: 500,
+};
---
+++ client/src/app/introduce/components/section2.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import { color, motion, Variants } from "framer-motion";
+import { CommonContainer, MainContentBox, MainContentText } from "../styles";
+import { Box } from "@mui/material";
+import MenuBar from "./MenuBar";
+
+// menubar framer
+
+const Section2: React.FC = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const sectionRef = useRef<HTMLDivElement | null>(null);
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
+  const words = [
+    "저희는 고객님들의",
+    "니즈를 고려하여",
+    "좋은 상품을",
+    "제공드리고 있습니다.",
+  ];
+
+  const sequence = {
+    hidden: { opacity: 0 },
+    visible: (i: number) => ({
+      opacity: 1,
+      transition: {
+        delay: i * 0.5,
+      },
+    }),
+  };
+
+  const buttonVariant = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: words.length * 0.5,
+      },
+    },
+  };
+
+  useEffect(() => {
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting) {
+        setIsVisible(true);
+        if (observerRef.current) {
+          observerRef.current.disconnect();
+        }
+      }
+    };
+
+    observerRef.current = new IntersectionObserver(handleIntersection, {
+      threshold: 0.1,
+    });
+
+    if (sectionRef.current) {
+      observerRef.current.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
+  }, []);
+
+  return (
+    <CommonContainer ref={sectionRef}>
+      <MainContentBox>
+        <MainContentText>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+            }}
+          >
+            {words.map((word, index) => (
+              <motion.div
+                key={index}
+                custom={index}
+                initial="hidden"
+                animate={isVisible ? "visible" : "hidden"}
+                variants={sequence}
+                style={{
+                  marginRight: "8px",
+                }}
+              >
+                {word}
+              </motion.div>
+            ))}
+          </Box>
+        </MainContentText>
+        <motion.div
+          // variants={buttonVariant}
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <MenuBar />
+        </motion.div>
+      </MainContentBox>
+    </CommonContainer>
+  );
+};
+
+export default Section2;
