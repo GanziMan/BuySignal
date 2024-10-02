61f68dcffad02e61365b1af0df8ffac192931cb0
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-02 14:33:37 +0900

    Fix: 빌드할때 eslint 무시

--- client/src/app/explore/[id]/page.tsx
+++ client/src/app/explore/[id]/page.tsx
@@ -2,11 +2,11 @@ import PageContainer from "@/components/PageContainer";
 import ExploreSearchForm from "./ExploreSearchForm";
 import NavBar from "@/components/NavBar";
 
-export async function generateMeta() {
-  return {
-    title: "",
-  };
-}
+// export async function generateMeta() {
+//   return {
+//     title: "",
+//   };
+// }
 
 export default async function ExploreSearchPage() {
   return (
