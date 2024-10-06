2c762f6278d0af3dfb61ea6491f11c7f5f3343ef
GanziMan <qjatn50089@gmail.com>
2024-10-07 00:43:02 +0900

    ADD: 공유하기 기능 추가

---
+++ client/src/components/ShareKakao.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Share2Icon } from "@radix-ui/react-icons";
+import { useEffect } from "react";
+
+interface LinkType {
+  mobileWebUrl: string;
+  webUrl: string;
+}
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+export default function ShareKakao({
+  title,
+  imageUrl,
+  link,
+}: {
+  title: string;
+  imageUrl: string;
+  link: LinkType;
+}) {
+  const handleShare = () => {
+    window.Kakao?.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "모바일 청첩장",
+        description: "호영이와 도현이의 결혼식을 초대합니다.",
+        imageUrl:
+          "https://ilotteshopping.com/myweddingstyle/image/main/img3.png",
+        link: {
+          // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+          mobileWebUrl: "https://developers.kakao.com",
+          webUrl: "https://developers.kakao.com",
+        },
+      },
+      social: {
+        commentCount: 45,
+        sharedCount: 845,
+      },
+      buttons: [
+        {
+          title: "웹으로 보기",
+          link: {
+            mobileWebUrl: "https://developers.kakao.com",
+            webUrl: "https://developers.kakao.com",
+          },
+        },
+        {
+          title: "앱으로 보기",
+          link: {
+            mobileWebUrl: "https://developers.kakao.com",
+            webUrl: "https://developers.kakao.com",
+          },
+        },
+      ],
+    });
+  };
+  useEffect(() => {
+    if (typeof window !== "undefined" && !window.Kakao) {
+      const script = document?.createElement("script");
+      script.src = "https://developers.kakao.com/sdk/js/kakao.min.js";
+      script.onload = () => {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY!);
+      };
+      document?.body.appendChild(script);
+    } else if (
+      typeof window !== "undefined" &&
+      window.Kakao &&
+      !window.Kakao.isInitialized()
+    ) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY!);
+    }
+  }, []);
+
+  return <Share2Icon width={25} height={25} onClick={handleShare} />;
+}
