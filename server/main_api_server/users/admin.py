802983b1efce1333cbcb624612235b7199e07382
Lee-Hwa-Jun <swag0090@naver.com>
2024-10-06 20:12:02 +0900

    jwt 토큰 적용 및 가입,로그인 구현
    users/sign-up/
    users/sign-in/

--- server/main_api_server/users/admin.py
+++ server/main_api_server/users/admin.py
@@ -1,42 +1,19 @@
 from django.contrib import admin
-from .models import Seller, Buyer, Coupon
+from .models import User
 
-# Seller Admin
-class SellerAdmin(admin.ModelAdmin):
-    list_display = ('username', 'seller_name', 'email', 'no_bizcode', 'no_social', 'created_at', 'updated_at')
-    search_fields = ('username', 'seller_name', 'email', 'no_bizcode', 'no_social')
+@admin.register(User)
+class OrderAdmin(admin.ModelAdmin):
+    # Admin 페이지에 표시할 필드 목록
+    list_display = ('login_id', 'email', 'password_hash', 'name', 'address', 'phone_number')
+    # Admin 페이지에서 필터할 필드
     list_filter = ('created_at', 'updated_at')
+    # 검색할 필드
+    search_fields = ('login_id', 'email', 'name', 'address')
+    # 읽기 전용 필드 (수정 불가)
     readonly_fields = ('created_at', 'updated_at')
-
-# Buyer Admin
-class BuyerAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'point', 'created_at', 'updated_at')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('created_at', 'updated_at')
-    readonly_fields = ('created_at', 'updated_at')
-
-# # Cart Admin
-# class CartAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'product', 'quantity', 'created_at', 'updated_at')
-#     search_fields = ('user__username', 'product__name')
-#     list_filter = ('created_at', 'updated_at')
-#     readonly_fields = ('created_at', 'updated_at')
-
-# # Like Admin
-# class LikeAdmin(admin.ModelAdmin):
-#     list_display = ('user',)
-#     search_fields = ('user__username',)
-#     filter_horizontal = ('product_id',)  # For many-to-many relationship display
-
-# Coupon Admin
-class CouponAdmin(admin.ModelAdmin):
-    list_display = ('coupon_id', 'discount_amount', 'discount_rate', 'used', 'used_id')
-    search_fields = ('coupon_id', 'used_id__username')
-    list_filter = ('used',)
-
-# Register the models in the admin site
-admin.site.register(Seller, SellerAdmin)
-admin.site.register(Buyer, BuyerAdmin)
-# admin.site.register(Cart, CartAdmin)
-# admin.site.register(Like, LikeAdmin)
-admin.site.register(Coupon, CouponAdmin)
+    # 필드 분류
+    # fieldsets = (
+    #     (None, {'fields': ('user', 'status', 'total_amount')}),
+    #     ('Addresses', {'fields': ('shipping_address', 'billing_address')}),
+    #     ('Timestamps', {'fields': ('created_at', 'updated_at')}),
+    # )
