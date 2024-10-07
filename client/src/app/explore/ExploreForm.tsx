d7e18131239c51f8553e9ec0940f0b9598162c9b
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-07 12:47:10 +0900

    Fix: Explore Category 추가

--- client/src/app/explore/ExploreForm.tsx
+++ client/src/app/explore/ExploreForm.tsx
@@ -2,7 +2,9 @@
 
 import Header from "@/components/Header";
 import SearchComponent from "@/components/SearchComponent";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const mockExploreData = [
   {},
@@ -20,8 +22,36 @@ const mockExploreData = [
   {},
   {},
 ];
+
+const categoryButtonData = [
+  {
+    url: "all",
+    name: "전체",
+  },
+  {
+    url: "clothes",
+    name: "옷",
+  },
+  {
+    url: "shoes",
+    name: "신발",
+  },
+];
 export default function ExploreForm() {
   const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const searchCategory = searchParams.get("category");
+
+  const [category, setCategory] = useState<
+    "all" | "clothes" | "shoes" | string
+  >("all");
+
+  useEffect(() => {
+    setCategory(searchCategory!);
+  }, [searchCategory]);
+
+  console.log(category);
   return (
     <div className="w-full h-full flex flex-col gap-5 py-2">
       <div className="flex flex-col gap-[30px] items-center">
@@ -29,6 +59,21 @@ export default function ExploreForm() {
         <SearchComponent />
       </div>
 
+      <div className="grid grid-cols-3 gap-[15px] w-full h-full max-w-[364px]">
+        {categoryButtonData.map((item) => {
+          return (
+            <div
+              onClick={() => router.push(`/explore?category=${item.url}`)}
+              className={twMerge(
+                "flex flex-col gap-7 justify-center items-center max-w-[174px] w-full h-[80px] border border-zinc-300 rounded-xl cursor-pointer hover:bg-slate-200",
+                category === item.url ? "bg-slate-200" : ""
+              )}
+            >
+              <p className="text-base font-bold">{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
       <div className="grid grid-cols-2 gap-[15px] w-full h-full max-w-[364px]">
         {mockExploreData.map((item) => {
           return (
