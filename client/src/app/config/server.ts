9eeb509156fd4edabe1ab6c18cd065298c158cdf
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-14 18:30:14 +0900

    ADD: 서버 환경 변수 작업

---
+++ client/src/app/config/server.ts
@@ -0,0 +1,13 @@
+import "server-only";
+
+export const ACCESS_TOKEN_SECRET = new TextEncoder().encode(
+  process.env.ACCESS_TOKEN_SECRET
+);
+
+export const REFRESH_TOKEN_SECRET = new TextEncoder().encode(
+  process.env.REFRESH_TOKEN_SECRET
+);
+
+export const ACCESS_TOKEN_EXPIRY = "15m";
+
+export const REFRESH_TOKEN_EXPIRY = "7d";
