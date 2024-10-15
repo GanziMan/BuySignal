6b225c497a0788e4c9778e7c5239fa54e4d607fe
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-15 15:58:14 +0900

    Update: middleware 토큰이 있을 경우 signin signup 접근 금지

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -8,63 +8,62 @@ import {
   REFRESH_TOKEN_SECRET,
 } from "./app/config/server";
 import { JWTExpired } from "jose/errors";
-export const PUBLIC_PATHS = ["/account/login", "/account/signup"];
+
 export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  const isPublic = PUBLIC_PATHS.some((path) => pathname.startsWith(path));
 
-  // Allow access to public paths
-  if (isPublic) {
+  //토큰처리
+  const accessToken = req.cookies.get("accessToken");
+  const refreshToken = req.cookies.get("refreshToken");
+
+  if (!accessToken?.value) {
+    if (pathname !== "/account/signin" && pathname !== "/account/signup") {
+      return NextResponse.redirect(DOMAIN_URL + "/account/signin");
+    }
     return NextResponse.next();
   }
 
-  if (!isPublic) {
-    //토큰처리
-    const accessToken = req.cookies.get("accessToken");
-    const refreshToken = req.cookies.get("refreshToken");
-
-    if (!accessToken?.value) {
-      if (pathname !== "/account/signin") {
-        return NextResponse.redirect(DOMAIN_URL + "/account/signin");
-      }
-      return NextResponse.next();
+  try {
+    await jwtVerify(accessToken.value, ACCESS_TOKEN_SECRET);
+    if (pathname === "/account/signin") {
+      return NextResponse.redirect(DOMAIN_URL + "/main");
     }
 
-    try {
-      await jwtVerify(accessToken.value, ACCESS_TOKEN_SECRET);
-    } catch (error) {
-      if (error instanceof JWTExpired && refreshToken?.value != null) {
-        try {
-          const verified = await jwtVerify(
-            refreshToken.value,
-            REFRESH_TOKEN_SECRET
-          );
-          const newAccessToken = await new SignJWT(verified.payload)
-            .setProtectedHeader({ alg: "HS256", typ: "JWT" })
-            .setIssuedAt()
-            .setExpirationTime(ACCESS_TOKEN_EXPIRY)
-            .sign(ACCESS_TOKEN_SECRET);
+    if (pathname === "/account/signup") {
+      return NextResponse.redirect(DOMAIN_URL + "/main");
+    }
+  } catch (error) {
+    if (error instanceof JWTExpired && refreshToken?.value != null) {
+      try {
+        const verified = await jwtVerify(
+          refreshToken.value,
+          REFRESH_TOKEN_SECRET
+        );
+        const newAccessToken = await new SignJWT(verified.payload)
+          .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+          .setIssuedAt()
+          .setExpirationTime(ACCESS_TOKEN_EXPIRY)
+          .sign(ACCESS_TOKEN_SECRET);
 
-          const newRefreshToken = await new SignJWT(verified.payload)
-            .setProtectedHeader({ alg: "HS256", typ: "JWT" })
-            .setIssuedAt()
-            .setExpirationTime(REFRESH_TOKEN_EXPIRY)
-            .sign(REFRESH_TOKEN_SECRET);
+        const newRefreshToken = await new SignJWT(verified.payload)
+          .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+          .setIssuedAt()
+          .setExpirationTime(REFRESH_TOKEN_EXPIRY)
+          .sign(REFRESH_TOKEN_SECRET);
 
-          req.cookies.set("accessToken", newAccessToken);
-          req.cookies.set("refreshToken", newRefreshToken);
+        req.cookies.set("accessToken", newAccessToken);
+        req.cookies.set("refreshToken", newRefreshToken);
 
-          const res = NextResponse.next({ request: req });
-          res.cookies.set("accessToken", newAccessToken);
-          res.cookies.set("refreshToken", newRefreshToken);
+        const res = NextResponse.next({ request: req });
+        res.cookies.set("accessToken", newAccessToken);
+        res.cookies.set("refreshToken", newRefreshToken);
 
-          return res;
-        } catch (error) {
-          return NextResponse.redirect(DOMAIN_URL + "/account/signin");
-        }
-      } else {
+        return res;
+      } catch (error) {
         return NextResponse.redirect(DOMAIN_URL + "/account/signin");
       }
+    } else {
+      return NextResponse.redirect(DOMAIN_URL + "/account/signin");
     }
   }
 
