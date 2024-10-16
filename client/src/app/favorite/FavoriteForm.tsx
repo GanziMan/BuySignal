87891d857876a8a08fd4ca2a4e4363c91d78c138
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-16 19:21:55 +0900

    Fix:: 찜 페이지 수정

--- client/src/app/favorite/FavoriteForm.tsx
+++ client/src/app/favorite/FavoriteForm.tsx
@@ -58,7 +58,7 @@ export default function FavoriteForm() {
       </div>
       <nav className="bg-[#489E67] text-white max-w-[344px] w-full h-[57px] bottom-[117px] fixed  rounded-2xl flex justify-center items-center z-[1000px]">
         <p className="text-lg font-semibold">장바구니 담기</p>
-        {/* <p className="float-right">총 금액</p> */}
+        <p className="float-right">총 금액</p>
       </nav>
     </section>
   );
