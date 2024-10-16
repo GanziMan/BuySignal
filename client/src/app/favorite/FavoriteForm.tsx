cee951c55c5d56681e99398c4febfa9592bd0062
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-10-16 19:24:02 +0900

    Fix: FavoriteForm 작업

--- client/src/app/favorite/FavoriteForm.tsx
+++ client/src/app/favorite/FavoriteForm.tsx
@@ -4,22 +4,17 @@ import { Separator } from "@radix-ui/react-separator";
 import Image from "next/image";
 
 const mockFavoriteData = [
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
+  { name: "나이키 에어맥스 운동화", price: 5000 },
+  { name: "나이키 에어맥스 운동화", price: 5000 },
+  { name: "나이키 에어맥스 운동화", price: 5000 },
+  { name: "나이키 에어맥스 운동화", price: 5000 },
+  { name: "나이키 에어맥스 운동화", price: 5000 },
+  { name: "나이키 에어맥스 운동화", price: 5000 },
+  { name: "나이키 에어맥스 운동화", price: 5000 },
+  { name: "나이키 에어맥스 운동화", price: 5000 },
 ];
+
+const totalPrice = mockFavoriteData.reduce((acc, item) => acc + item.price, 0);
 
 export default function FavoriteForm() {
   return (
@@ -42,10 +37,8 @@ export default function FavoriteForm() {
                 <div className="flex flex-col w-[260px] gap-4">
                   <div className="flex items-center justify-between">
                     <div className="flex flex-col gap-[5px]">
-                      <p className="text-base font-bold">
-                        나이키 운동화 에어맥스
-                      </p>
-                      <p className="font-semibold text-sm">110,000원</p>
+                      <p className="text-base font-bold">{item.name}</p>
+                      <p className="font-semibold text-sm">{item.price}원</p>
                     </div>
 
                     <ChevronRightIcon width={20} height={20} />
@@ -58,7 +51,7 @@ export default function FavoriteForm() {
       </div>
       <nav className="bg-[#489E67] text-white max-w-[344px] w-full h-[57px] bottom-[117px] fixed  rounded-2xl flex justify-center items-center z-[1000px]">
         <p className="text-lg font-semibold">장바구니 담기</p>
-        <p className="float-right">총 금액</p>
+        <p className="float-right">총 금액({totalPrice}원)</p>
       </nav>
     </section>
   );
