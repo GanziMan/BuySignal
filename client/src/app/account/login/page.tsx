07e3cd6483c2ce64f3aac1e87220f901705424a2
GanziMan <qjatn50089@gmail.com>
2024-10-26 23:56:26 +0900

    ADD: LoginPage

---
+++ client/src/app/account/login/page.tsx
@@ -0,0 +1,6 @@
+import PageContainer from "@/components/PageContainer";
+import LoginForm from "./LoginForm";
+
+export default function LoginPage() {
+  return <PageContainer children={<LoginForm></LoginForm>} />;
+}
