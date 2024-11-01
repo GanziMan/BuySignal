b29d5536ce9b48906e9285e4cafe9f17571c2620
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-01 18:20:49 +0900

    Update: signUpSchema gender 추가

--- client/src/app/account/signup/actions/signUpSchema.ts
+++ client/src/app/account/signup/actions/signUpSchema.ts
@@ -4,6 +4,7 @@ export const signUpSchema = z.object({
   email: z.string(),
   password: z.string(),
   name: z.string(),
+  gender: z.string(),
 });
 
 export type SignUpRequest = z.input<typeof signUpSchema>;
