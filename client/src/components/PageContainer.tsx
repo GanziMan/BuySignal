c85cc77a390cc5f7c84832f3c6c1bf00dd294c2d
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-01 18:53:33 +0900

    Fix: PageContainer css 수정 (h-screen 추가)

--- client/src/components/PageContainer.tsx
+++ client/src/components/PageContainer.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function PageContainer({ children }: { children: ReactNode }) {
   return (
-    <main className="w-full  flex justify-center bg-slate-100">
+    <main className="w-full h-screen  flex justify-center bg-slate-100">
       <section className="max-w-[600px] w-full h-full bg-white flex justify-center pb-[100px]">
         <div className="max-w-[364px] w-full h-full">{children}</div>
       </section>
